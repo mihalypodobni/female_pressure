fb459a535663806c712f34281e52dcfcf68d9ba9
Alex Dohm <dohma@oregonstate.edu>
2021-05-02 20:59:28 +0200

    formatting

--- src/components/specialProjects/ProjectCard.vue
+++ src/components/specialProjects/ProjectCard.vue
@@ -1,51 +1,51 @@
 <template>
-    <div>
-        <b-container > 
-        <b-row>
-            <!-- PROJECT CONTAINER -->
-            <b-col :key="project.id" v-for="project in projects" cols="12" md="6" >
-                <b-container class="project-container mt-5 mb-5" >
-                    <b-row >
-                    <b-col cols="4">
-                        <b-img :src="project.img" alt="common planet logo" class="project-img" ></b-img>
-                    </b-col>
+  <div>
+    <b-container>
+      <b-row>
+        <!-- PROJECT CONTAINER -->
+        <b-col :key="project.id" v-for="project in projects" cols="12" md="6">
+          <b-container class="project-container mt-5 mb-5">
+            <b-row>
+              <b-col cols="4">
+                <b-img :src="project.img" alt="common planet logo" class="project-img"></b-img>
+              </b-col>
 
-                    <b-col cols="8">
-                        <h3>{{project.title}}</h3>
-                        <p>{{project.text}}</p>
-                        <b-link :src="project.link" class="link" target="_blank" >{{project.link}}</b-link>
+              <b-col cols="8">
+                <h3>{{ project.title }}</h3>
+                <p>{{ project.text }}</p>
+                <b-link :src="project.link" class="link" target="_blank">{{ project.link }}</b-link>
 
-                        <!-- SOCIAL MEDIA LINKS -->
-                            <b-row>
-                                <b-button variant="link" 
-                                :href="link.link" 
-                                target="_blank" 
-                                v-for="(link,idx) in project.socialMedia" 
-                                :key="link.icon + idx"
-                                class="p-1 link">
-                                    <font-awesome-icon :icon="['fab', link.icon]" size="lg" class="mx-2 my-2"/>
-                                </b-button>
-                            </b-row>
-                        
-                    </b-col>
-                    </b-row>
-                </b-container>
-            </b-col>
-        </b-row>
-        </b-container>
-    </div>
-   
+                <!-- SOCIAL MEDIA LINKS -->
+                <b-row>
+                  <b-button variant="link"
+                            :href="link.link"
+                            target="_blank"
+                            v-for="(link,idx) in project.socialMedia"
+                            :key="link.icon + idx"
+                            class="p-1 link">
+                    <font-awesome-icon :icon="['fab', link.icon]" size="lg" class="mx-2 my-2"/>
+                  </b-button>
+                </b-row>
+
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+
 </template>
 
 
 <script>
 export default {
-    props:{
-        projects: Array
-    },
-    computed: {
-        console: () => console,
-    }
+  props: {
+    projects: Array
+  },
+  computed: {
+    console: () => console,
+  }
 }
 </script>
 
@@ -54,11 +54,11 @@ export default {
   border: 2px solid $borderlink
   padding: 30px
   box-shadow: 0px 0px 15px grey
-  
+
 .link
   @include blue-link
 
 .project-img
-    border-radius: 50%
-    width: 100%
+  border-radius: 50%
+  width: 100%
 </style>
--- src/components/specialProjects/SpecialProjectsHome.vue
+++ src/components/specialProjects/SpecialProjectsHome.vue
@@ -1,91 +1,92 @@
 <template>
- <main>
-   <b-container class="mt-3"> 
-    <h1 class="page-title">Special projects</h1>
-    <ProjectCard :projects="projectsData"/>
-   </b-container>
-    
+  <main>
+    <b-container class="mt-3">
+      <h1 class="page-title">Special projects</h1>
+      <ProjectCard :projects="projectsData"/>
+    </b-container>
+
   </main>
 </template>
 
 <script>
 import ProjectCard from "./ProjectCard"
+
 export default {
-  name:"SpecialProjectsHome",
-  components:{
+  name: "SpecialProjectsHome",
+  components: {
     ProjectCard
   },
 
   data() {
-    return{
-    projectsData:[
-      {
-        img:"https://picsum.photos/200", 
-        title:"common planet",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      },
-      {
-        img:"https://picsum.photos/200", 
-        title:"female:pressure podcast",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      },
-      {
-        img:"https://picsum.photos/200", 
-        title:"rojava compilation",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      },
-      {
-        img:"https://picsum.photos/200", 
-        title:"facts",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      },
-      {
-        img:"https://picsum.photos/200", 
-        title:"visibility blog",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      },
-      {
-        img:"https://picsum.photos/200", 
-        title:"pussy riot freedom compilation",
-        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
-        link:"https://open.currents.fm/g/femalepressure",
-        socialMedia: [
-        {icon: "twitter", link: "https://twitter.com/female_pressure"},
-        {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
-        {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
-        ]
-      }
-    ]
+    return {
+      projectsData: [
+        {
+          img: "https://picsum.photos/200",
+          title: "common planet",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        },
+        {
+          img: "https://picsum.photos/200",
+          title: "female:pressure podcast",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        },
+        {
+          img: "https://picsum.photos/200",
+          title: "rojava compilation",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        },
+        {
+          img: "https://picsum.photos/200",
+          title: "facts",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        },
+        {
+          img: "https://picsum.photos/200",
+          title: "visibility blog",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        },
+        {
+          img: "https://picsum.photos/200",
+          title: "pussy riot freedom compilation",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+          link: "https://open.currents.fm/g/femalepressure",
+          socialMedia: [
+            {icon: "twitter", link: "https://twitter.com/female_pressure"},
+            {icon: "facebook", link: "https://www.facebook.com/femalepressure"},
+            {icon: "soundcloud", link: "https://soundcloud.com/femalepressure"},
+          ]
+        }
+      ]
     }
   },
 }
@@ -94,6 +95,6 @@ export default {
 
 <style lang="sass" scoped>
 .page-title
-  @include  page-title
+  @include page-title
 
 </style>
