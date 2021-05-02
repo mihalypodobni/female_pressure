dfdbf082f8938cf5ce08387b51a2531378a44159
INes <inesfcnogueira@gmail.com>
2021-05-02 22:29:41 +0200

    first commti about home

--- src/components/about/AboutHome.vue
+++ src/components/about/AboutHome.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>About page</div>
+  <b-container class="mt-5">
+      <h3>visibility</h3>
+      <p>female pressure is a transnational network of database and network of women*, AFAB, transgender, transfeminine, transmasculine, intersex (+gender optional), genderqueer, gender nonconforming, a-gender or/and non-binary artists in electronic music and digital arts founded by Electric Indigo: from musicians, composers and DJs to visual artists, cultural workers and researchers. A worldwide resource of talent that can be searched after criteria like location, profession, style or name. "Why are there so few women active in the electronic music scene?" Each one of us has heard this question a thousand times... Here is the answer: It's not about a total number, it's about how and if we are recognized!</p>
+
+      <h3>vision</h3>
+      <p> female:pressure intends to strengthen networking, communication and representation - a standard instrument to obtain information about artists, contact them, and find out about other, maybe less known women in electronic music all around the globe. Every artist listed in this database is able to keep their personal entry up-to-date. The network comprises of over 2700 members from 81 countries as at February 2021. If you want to be part of female:pressure don't hesitate to send an e-mail to: infoATfemalepressureDOTnet.<br/>
+      You never walk alone!
+      </p>
+
+      <h3>press</h3>
+      <p>further information about female:pressure [english and german versions]</p>
+      <p>#femalepressure - our press statement from march 8 2013</p>
+      <p>our press review [as of 2013]</p>
+      <p>some pictures and the logo...</p>
+
+      <h3>credits</h3>
+      <p>this service is provided by electric indigo</p>
+      <p>female pressure logo design by datz@sil.at</p>
+
+	
+    </b-container>
 </template>
