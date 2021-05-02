1c434b6960484d9f647c8be179427bd755f08587
INes <inesfcnogueira@gmail.com>
2021-05-02 19:40:28 +0200

    first commit special projects

---
+++ src/components/specialProjects/ProjectCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <b-container > 
+        <b-row>
+            <!-- PROJECT CONTAINER -->
+            <b-col :key="project.id" v-for="project in projects" cols="12" md="6" >
+                <b-container class="project-container mt-5 mb-5" >
+                    <b-row >
+                    <b-col cols="4">
+                        <b-img :src="project.img" alt="common planet logo" class="project-img" ></b-img>
+                    </b-col>
+
+                    <b-col cols="8">
+                        <h3>{{project.title}}</h3>
+                        <p>{{project.text}}</p>
+                        <b-link>{{project.link}}</b-link>
+                    </b-col>
+                    </b-row>
+                </b-container>
+            </b-col>
+        </b-row>
+        </b-container>
+    </div>
+   
+</template>
+
+
+<script>
+export default {
+    props:{
+        projects: Array
+    },
+}
+</script>
+console.log(projects);
+
+<style lang="sass" scoped>
+.project-container
+  border: 2px solid #3dffc5
+  padding: 30px
+  box-shadow: 0px 0px 15px grey
+  
+
+.project-img
+    border-radius: 50%
+    width: 100%
+</style>
--- src/components/specialProjects/SpecialProjectsHome.vue
+++ src/components/specialProjects/SpecialProjectsHome.vue
@@ -1,3 +1,63 @@
 <template>
-  <div>This is the projects page</div>
+ <main>
+   <b-container class="mt-3"> 
+    <h1>Special projects</h1>
+    <ProjectCard :projects="projectsData"/>
+   </b-container>
+    
+  </main>
 </template>
+
+<script>
+import ProjectCard from "./ProjectCard"
+export default {
+  name:"SpecialProjectsHome",
+  components:{
+    ProjectCard
+  },
+
+  data() {
+    return{
+    projectsData:[
+      {
+        img:"https://picsum.photos/200", 
+        title:"common planet",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      },
+      {
+        img:"https://picsum.photos/200", 
+        title:"female:pressure podcast",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      },
+      {
+        img:"https://picsum.photos/200", 
+        title:"rojava compilation",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      },
+      {
+        img:"https://picsum.photos/200", 
+        title:"facts",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      },
+      {
+        img:"https://picsum.photos/200", 
+        title:"visibility blog",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      },
+      {
+        img:"https://picsum.photos/200", 
+        title:"pussy riot freedom compilation",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse nam aliquid exercitationem tempore architecto? Itaque nostrum impedit fugit asperiores.",
+        link:"https://open.currents.fm/g/femalepressure",
+      }
+    ]
+    }
+  },
+}
+</script>
+
