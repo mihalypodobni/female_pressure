192e390c58c5fd489c1f6ad1f8c4a6b13e8a020b
inesfcnogueira@gmail.com <inesfcnogueira@gmail.com>
2021-05-10 22:56:54 +0200

    updated links design

--- src/components/links/LinksHome.vue
+++ src/components/links/LinksHome.vue
@@ -1,148 +1,180 @@
 <template>
-	<b-container class="mt-5">
-		<div class="accordion" role="tablist">
-			<b-card
-				no-body
-				class="mb-1"
-				v-for="(category, idx) in linksData"
-				:key="category"
-			>
-				<b-card-header class="accordion-container" role="tab">
-					<!-- CATEGORY TITLE -->
-					<b-card-text
-						block
-						v-b-toggle="'accordion-' + idx"
-						class="accordion-button"
-					>
-						{{ category.categoryTitle }}
-					</b-card-text>
-				</b-card-header>
+  <b-container class="mt-5">
+    <div class="accordion" role="tablist">
+      <b-card
+        no-body
+        class="mb-1 main-card"
+        v-for="(category, idx) in linksData"
+        :key="category"
+      >
+        <b-card-header class="accordion-container" role="tab">
+          <!-- CATEGORY TITLE -->
+          <b-card-text
+            block
+            v-b-toggle="'accordion-' + idx"
+            class="accordion-button"
+          >
+            <h5>
+              <b>{{ category.categoryTitle }}</b>
+            </h5>
+          </b-card-text>
+        </b-card-header>
 
-				<!-- LINKS -->
-				<b-collapse
-					:id="'accordion-' + idx"
-					accordion="my-accordion"
-					role="tabpanel"
-				>
-					<b-card-body v-for="link in category.links" :key="link.title">
-						<b-link :src="link.url" class="link" target="_blank">{{
-							link.title
-						}}</b-link>
-					</b-card-body>
-				</b-collapse>
-			</b-card>
-		</div>
-	</b-container>
+        <!-- LINKS -->
+        <b-collapse
+          visible
+          :id="'accordion-' + idx"
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body
+            v-for="link in category.links"
+            :key="link.title"
+            class="link-container d-flex justify-content-between align-items-end"
+          >
+            <b-link :src="link.url" class="link" target="_blank">{{
+              link.title
+            }}</b-link>
+            <p class="m-0">{{ link.language }}</p>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+  </b-container>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				linksData: [
-					{
-						categoryTitle: "anti-racism reading and listening resources",
-						links: [
-							{
-								title: "#blacklivesmatter community resource console",
-								url: "https://blm.currents.fm/k",
-							},
-							{
-								title: "anti-racism resources",
-								url:
-									"https://docs.google.com/document/d/1ko1dRVSUtpDntIZ5SrewALLhcMDuhe69Om31oOHIkh8/",
-							},
-							{
-								title: "anti-racism resources for white people",
-								url:
-									"https://docs.google.com/document/d/1BRlF2_zhNe86SGgHa6-VlBO-QgirITwCTugSfKie5Fs/edit",
-							},
-							{
-								title: "black lives matter - ressourcen auf deutsch",
-								url:
-									"https://docs.google.com/document/d/1BZk5sDngE8Z8twGzzbmAg78ful3zCGlzZ9W73Pguw-Y/edit",
-							},
-							{
-								title: "how to support black lives in the uk",
-								url:
-									"https://docs.google.com/document/d/10iIz_pFB8DzPkwddc8dcmJdJ0ZMITfTs7lvs4uyusZk/edit",
-							},
-							{
-								title:
-									"links, ressourcen, texte, personen, organisationen in deutschland",
-								url:
-									"https://docs.google.com/document/d/1BZk5sDngE8Z8twGzzbmAg78ful3zCGlzZ9W73Pguw-Y/edit",
-							},
-							{
-								title: "techno and its history",
-								url: "https://dwellerforever.blog/library/",
-							},
-							{
-								title:
-									"what does it mean to be antiracist? how do we overcome white supremacy?",
-								url:
-									"https://multcolib.bibliocommons.com/list/share/114633184/1648115099",
-							},
-						],
-					},
-					{
-						categoryTitle: "archives of female:pressure radio shows",
-						links: [
-							{
-								title: "female:pressure @ orange 94.0 / vienna",
-								url: "https://soundcloud.com/femalepressure-orange940",
-							},
-							{
-								title: "female frequencies @ reboot.fm / berlin",
-								url:
-									"https://cargocollective.com/female_frequencies/previous-shows/",
-							},
-							{
-								title: "female:pressure @ frission radio / ireland",
-								url:
-									"https://www.mixcloud.com/frission/playlists/femalepressure-de/",
-							},
-							{
-								title: "female:pressure @ byte.fm / hamburg",
-								url: "https://www.byte.fm/sendungen/femalepressure/",
-							},
-							{
-								title: "female:pressure radio @ 674.fm / cologne",
-								url: "https://www.mixcloud.com/femalepressureradio/",
-							},
-							{
-								title: "female:pressure @ refuge worldwide",
-								url: "https://refugeworldwide.com/artists/female-pressure",
-							},
-							{
-								title: "female:pressure @ resonance extra / london",
-								url: "https://extra.resonance.fm/series/female-pressure",
-							},
-							{
-								title: "female:pressure @ resonancias @ radio 3 rtve / spain",
-								url: "www.rtve.es/alacarta/audios/resonancias/",
-							},
-							{
-								title: "doctore xyramat @ fsk / hamburg [playlists only]",
-								url: "https://www.fsk-hh.org/sendungen/doctore_xyramat",
-							},
-						],
-					},
-				],
-			};
-		},
-	};
+export default {
+  data() {
+    return {
+      linksData: [
+        {
+          categoryTitle: "anti-racism reading and listening resources",
+          links: [
+            {
+              title: "#blacklivesmatter community resource console",
+              url: "https://blm.currents.fm/k",
+              language: "EN",
+            },
+            {
+              title: "anti-racism resources",
+              url:
+                "https://docs.google.com/document/d/1ko1dRVSUtpDntIZ5SrewALLhcMDuhe69Om31oOHIkh8/",
+              language: "EN",
+            },
+            {
+              title: "anti-racism resources for white people",
+              url:
+                "https://docs.google.com/document/d/1BRlF2_zhNe86SGgHa6-VlBO-QgirITwCTugSfKie5Fs/edit",
+              language: "EN",
+            },
+            {
+              title: "black lives matter - ressourcen auf deutsch",
+              url:
+                "https://docs.google.com/document/d/1BZk5sDngE8Z8twGzzbmAg78ful3zCGlzZ9W73Pguw-Y/edit",
+              language: "DE",
+            },
+            {
+              title: "how to support black lives in the uk",
+              url:
+                "https://docs.google.com/document/d/10iIz_pFB8DzPkwddc8dcmJdJ0ZMITfTs7lvs4uyusZk/edit",
+              language: "EN",
+            },
+            {
+              title:
+                "links, ressourcen, texte, personen, organisationen in deutschland",
+              url:
+                "https://docs.google.com/document/d/1BZk5sDngE8Z8twGzzbmAg78ful3zCGlzZ9W73Pguw-Y/edit",
+              language: "DE",
+            },
+            {
+              title: "techno and its history",
+              url: "https://dwellerforever.blog/library/",
+              language: "EN",
+            },
+            {
+              title:
+                "what does it mean to be antiracist? how do we overcome white supremacy?",
+              url:
+                "https://multcolib.bibliocommons.com/list/share/114633184/1648115099",
+              language: "EN",
+            },
+          ],
+        },
+        {
+          categoryTitle: "archives of female:pressure radio shows",
+          links: [
+            {
+              title: "female:pressure @ orange 94.0 / vienna",
+              url: "https://soundcloud.com/femalepressure-orange940",
+              language: "EN",
+            },
+            {
+              title: "female frequencies @ reboot.fm / berlin",
+              url:
+                "https://cargocollective.com/female_frequencies/previous-shows/",
+              language: "EN",
+            },
+            {
+              title: "female:pressure @ frission radio / ireland",
+              url:
+                "https://www.mixcloud.com/frission/playlists/femalepressure-de/",
+              language: "EN",
+            },
+            {
+              title: "female:pressure @ byte.fm / hamburg",
+              url: "https://www.byte.fm/sendungen/femalepressure/",
+              language: "DE",
+            },
+            {
+              title: "female:pressure radio @ 674.fm / cologne",
+              url: "https://www.mixcloud.com/femalepressureradio/",
+              language: "DE",
+            },
+            {
+              title: "female:pressure @ refuge worldwide",
+              url: "https://refugeworldwide.com/artists/female-pressure",
+              language: "EN",
+            },
+            {
+              title: "female:pressure @ resonance extra / london",
+              url: "https://extra.resonance.fm/series/female-pressure",
+              language: "EN",
+            },
+            {
+              title: "female:pressure @ resonancias @ radio 3 rtve / spain",
+              url: "www.rtve.es/alacarta/audios/resonancias/",
+              language: "SP",
+            },
+            {
+              title: "doctore xyramat @ fsk / hamburg [playlists only]",
+              url: "https://www.fsk-hh.org/sendungen/doctore_xyramat",
+              language: "DE",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-	.accordion-container
-		border: 2px solid $blueBoxBorder
-		padding: 30px
-		box-shadow: 0px 0px 15px grey
-		height: 50px
-		display: flex
-		align-items: center
+.accordion-container
+	border-bottom: 2px solid $blueBoxBorder
+	background-color: white
+	padding: 5px
+	display: flex
+	align-items: center
 
-	.link
-		@include link
+.main-card
+	border: none
+
+.link-container
+	border-bottom: 1px solid $blueBoxBorder
+	padding: 5px
+
+
+.link
+	@include link
 </style>
