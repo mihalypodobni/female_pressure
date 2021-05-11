89d359c18da439b108e3dcb9daae174b81458173
Alex Dohm <dohma@oregonstate.edu>
2021-05-11 21:43:18 +0200

    started user login

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import {library} from '@fortawesome/fontawesome-svg-core'
+
 import {
     faTwitter,
     faFacebook,
@@ -23,6 +25,12 @@ library.add(faTwitter, faFacebook, faSoundcloud, faMixcloud, faInstagram, faVime
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+Vue.prototype.$hostname = process.env.NODE_ENV === 'development' ?
+    `${window.location.protocol}//${window.location.hostname}:8080` :
+    `${window.location.protocol}//${window.location.host}`
+
 
 new Vue({
     store,
