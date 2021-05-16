87167198371da1c775fd48773b084e4e504336d3
Alex Dohm <dohma@oregonstate.edu>
2021-05-16 20:27:46 +0200

    logo is centered and working properly

--- src/components/navigation/NavigationDesktop.vue
+++ src/components/navigation/NavigationDesktop.vue
@@ -2,7 +2,7 @@
   <b-row class="justify-content-center w-100 mx-0">
     <b-col class="text-center link female_pressure_nav">
       <div class="logo-wrapper">
-      <b-img src="../../assets/fp-main-logo.svg" class="fp-logo"></b-img>
+      <b-img src="../../assets/fp-main-logo.svg" class="fp-logo" @click="$emit('navigate','home')"></b-img>
       </div>
     </b-col>
     <b-col v-for="(item, idx) in menuItems"
@@ -74,11 +74,17 @@ export default {
   width: 3rem
   max-width: 200px
   top: 10px
-  left: -100% + 100
+  @media screen and (max-width: 1399px)
+    left: -100% + 95
+  @media screen and (min-width: 1400px)
+    left: -100% + 100
 
 .fp-logo
   position: fixed
-  max-width: 150px
   z-index: 9
   border: 3px solid white
+  @media screen and (max-width: 1399px)
+    width: calc(100vw / 7 - 30px)
+  @media screen and (min-width: 1400px)
+    width: 150px
 </style>
