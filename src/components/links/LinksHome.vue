b2858248b4bf6d36652da1d4215e241267bcb9e5
inesfcnogueira@gmail.com <inesfcnogueira@gmail.com>
2021-05-19 15:56:10 +0200

    updated links design

--- src/components/links/LinksHome.vue
+++ src/components/links/LinksHome.vue
@@ -2,35 +2,33 @@
   <b-container class="mt-5 body-container">
     <div class="accordion" role="tablist">
       <b-card
-          no-body
-          class="mb-1 main-card"
-          v-for="(category, idx) in linksData"
-          :key="category.categoryTitle"
+        no-body
+        class="mb-1 main-card"
+        v-for="(category, idx) in linksData"
+        :key="category.categoryTitle"
       >
-        <b-card-header class="accordion-container" role="tab">
+        <b-card-header class="accordion-container mb-3" role="tab">
           <!-- CATEGORY TITLE -->
           <b-card-text
-              block
-              v-b-toggle="'accordion-' + idx"
-              class="accordion-button"
+            block
+            v-b-toggle="'accordion-' + idx"
+            class="accordion-button h5 py-1 px-2"
           >
-            <h5>
-              <b>{{ category.categoryTitle }}</b>
-            </h5>
+            <b>{{ category.categoryTitle }}</b>
           </b-card-text>
         </b-card-header>
 
         <!-- LINKS -->
         <b-collapse
-            visible
-            :id="'accordion-' + idx"
-            accordion="my-accordion"
-            role="tabpanel"
+          :id="'accordion-' + idx"
+          accordion="my-accordion"
+          role="tabpanel"
+          class="mb-3"
         >
           <b-card-body
-              v-for="link in category.links"
-              :key="link.title"
-              class="link-container d-flex justify-content-between align-items-end"
+            v-for="link in category.links"
+            :key="link.title"
+            class="link-container d-flex justify-content-between align-items-end"
           >
             <b-link :src="link.url" class="link" target="_blank">
               {{ link.title }}
@@ -44,28 +42,32 @@
 </template>
 
 <script>
-import {links} from "@/assets/data/links";
+import { links } from "@/assets/data/links";
 
 export default {
   data() {
     return {
-      linksData: links
-    }
+      linksData: links,
+    };
   },
   mounted() {
-    console.log(this.$route.params.category)
-  }
+    console.log(this.$route.params.category);
+  },
 };
 </script>
 
 <style lang="sass" scoped>
 .accordion-container
-  border-bottom: 2px solid $blueBoxBorder
+  border: 2px solid $blueBoxBorder
   background-color: white
-  padding: 5px
+  padding: 5px 15px
   display: flex
   align-items: center
 
+
+.accordion-button
+  display: flex
+  align-items: center
 .main-card
   border: none
 
@@ -75,7 +77,7 @@ export default {
 
 .link-container:hover
   background-color: $blueBoxBorder
-  padding-left: 15px
+  // padding-left: 15px
 
 .link
   color: black
