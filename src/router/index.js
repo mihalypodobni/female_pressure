cc3612695d7347db8b19daae664f5253543d0f9a
Alex Dohm <dohma@oregonstate.edu>
2021-05-29 15:11:06 +0200

    fix bug - redunant navigatio error

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,13 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
+//avoid redundant navigation
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(() => {
+  });
+};
+
 let router = new Router({
   mode: "history",
   routes: [
