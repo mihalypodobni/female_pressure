42b3d36272f44b64c0f5e778f3d4272f5da53c12
Alex Dohm <dohma@oregonstate.edu>
2021-06-05 13:27:53 +0200

    typeahead now works properly

--- src/components/faq/FAQHome.vue
+++ src/components/faq/FAQHome.vue
@@ -1,20 +1,21 @@
 <template>
   <b-container class="body-container mt-3">
-    <vue-bootstrap-typeahead
+    <vue-typeahead-bootstrap
         :data="cities"
         v-model="citySearch"
         size="lg"
+        showAllResults
         :serializer="s => (s.address.label)"
-        placeholder="Type an city..."
+        placeholder="find a city..."
         @hit="selectedCity = $event"
-    ></vue-bootstrap-typeahead>
+    ></vue-typeahead-bootstrap>
     {{cities}}
   </b-container>
 </template>
 
 <script>
 import _ from 'underscore'
-import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+import VueTypeaheadBootstrap from 'vue-typeahead-bootstrap';
 
 const API_URL = 'https://autocomplete.search.hereapi.com/v1/autocomplete?q=:q&apiKey=lXXccHQOyrKXJVSglf0mwzxpzbjZNYw1aNTS46iS2k8&lang=en-US&types=city'
 
@@ -27,21 +28,19 @@ export default {
     }
   },
   components: {
-    VueBootstrapTypeahead
+    VueTypeaheadBootstrap
   },
   methods: {
     async getCities(query) {
-      console.log("trying to get cities")
       const res = await fetch(API_URL.replace(':q', query))
       const suggestions = await res.json()
       this.cities = suggestions.items
       console.log(this.cities)
     }
   },
+
   watch: {
-    citySearch: _.debounce(function (addr) {
-      this.getCities(addr)
-    }, 500)
+    citySearch: _.debounce(function(addr) { this.getCities(addr) }, 500)
   }
 }
 </script>
