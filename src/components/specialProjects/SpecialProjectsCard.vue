88676d63f9af420fc7aeaea7b1587633cfedce13
INes <inesfcnogueira@gmail.com>
2021-06-14 22:28:07 +0200

    fixed padding and layout of projects

--- src/components/specialProjects/SpecialProjectsCard.vue
+++ src/components/specialProjects/SpecialProjectsCard.vue
@@ -2,31 +2,32 @@
   <a :href="project.link" class="card-link link">
     <b-card no-body class="project-card px-3 py-2">
       <div class="card-body m-0">
-        <b-row>
-          <b-col class="p-0" cols="5" lg="4">
-            <!-- <div class="d-flex align-items-center"> -->
+        <b-row class="p-0 justify-content-start align-items-start">
+          <b-col class="p-0 h-100" cols="5" sm="3" md="2" lg="4">
             <b-img
               :src="require('@/assets/special-projects-logos/' + project.img)"
               :alt="project.title + ' logo'"
-              class="project-img mt-1 mr-2 p-0 ml-3"
+              class="project-img mt-1 mr-2 p-0"
             />
-            <!-- </div> -->
           </b-col>
-          <b-col cols="7" class="p-0 mh-50 mb-5">
-            <div class="ml-4 mt-1 p-0">
+          <b-col cols="7" class="p-0 mh-50 mb-2">
+            <div class="ml-3 mt-1 p-0">
               <h4 class="m-0">
-                {{ project.title }}
+                <b>{{ project.title }}</b>
               </h4>
               <p class="project-year mb-2">
                 <b>{{ project.year }}</b>
               </p>
+              <div v-if="!$store.state.isMobile" class="project-text p-0">
+                {{ project.text }}
+              </div>
             </div>
           </b-col>
-          <b-col cols="7" xl="9" class="p-0 mh-50">
-            <div class="project-text ml-4 mt-1 p-0">
-              {{ project.text }}
-            </div>
-          </b-col>
+        </b-row>
+        <b-row v-if="$store.state.isMobile">
+          <div class="project-text p-0">
+            {{ project.text }}
+          </div>
         </b-row>
       </div>
     </b-card>
@@ -45,31 +46,36 @@ export default {
 .project-card
   @include card-button
   border-radius: 0
+  border: none
 
 .project-img
   width: 100px
   height: 100px
-  @media screen and (max-width: 576px)
+  @media screen and (max-width: 1024px)
+    margin-bottom: 20px
     width: 80px
     height: 80px
 
 .project-year
   font-size: 15px
-  font-weight: 300
+  font-weight: 600
   color: $blueBoxBorder
 
 .project-text
-  font-weight: 600
+  font-weight: 400
 
 .card-link
   @include link
 
 .card-body
-  @media screen and (max-width: 1299px)
-    min-height: 330px
+  @media screen and (min-width: 992px)
+    min-height: 290px
     max-width: 380px
-  @media screen and (min-width: 1300px)
-    min-height: 305px
+  @media screen and (min-width: 1024px)
+    min-height: 385px
+    max-width: 450px
+  @media screen and (min-width: 1200px)
+    min-height: 308px
     max-width: 450px
 
 .card-link:hover
