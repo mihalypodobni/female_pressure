3ef5827e76fc3648fb5d060ac9d18889d56474c7
electricin <85707782+electricin@users.noreply.github.com>
2021-06-26 01:42:53 +0200

    updated faq page

--- src/components/faq/FAQHome.vue
+++ src/components/faq/FAQHome.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <Header />
+    <Header/>
     <b-container class="mt-5 body-container">
       <div class="accordion" role="tablist">
         <b-card
-          no-body
-          class="mb-1 main-card"
-          v-for="(question, idx) in faqData"
-          :key="question.title"
+            no-body
+            class="mb-1 main-card"
+            v-for="(question, idx) in faqData"
+            :key="question.title"
         >
           <b-card-header
-            class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-            role="tab"
-            v-b-toggle="'accordion-' + idx"
+              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
+              role="tab"
+              v-b-toggle="'accordion-' + idx"
           >
             <b class="question-title">{{ question.title }}</b>
           </b-card-header>
 
           <!-- LINKS -->
           <b-collapse
-            :id="'accordion-' + idx"
-            accordion="my-accordion"
-            role="tabpanel"
-            class="mb-3"
+              :id="'accordion-' + idx"
+              accordion="my-accordion"
+              role="tabpanel"
+              class="mb-3"
           >
             <p class="m-1">{{ question.answer }}</p>
           </b-collapse>
@@ -33,8 +33,8 @@
 </template>
 
 <script>
-import { faq } from "@/assets/data/faq";
-import { mapMutations } from "vuex";
+import {faq} from "@/assets/data/faq";
+import {mapMutations} from "vuex";
 
 export default {
   data() {
@@ -59,6 +59,7 @@ export default {
   display: flex
   align-items: center
   cursor: pointer
+
   &:hover
     background-color: #99fcc6
 
@@ -74,6 +75,7 @@ export default {
 .link-container
   border-bottom: 1px solid rgba(153, 252, 198, 0.5)
   padding: 5px
+
   &:hover
     background-color: #99fcc6
 
