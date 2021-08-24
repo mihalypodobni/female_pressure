4697fa6d0367cb10ddc28f46e8f8b2f1700541bf
Alex Dohm <dohma@oregonstate.edu>
2021-08-24 20:35:32 +0200

    small tweaks to menu after testing on mobile

--- src/components/navigation/mobile/NavigationMobile.vue
+++ src/components/navigation/mobile/NavigationMobile.vue
@@ -1,39 +1,40 @@
 <template>
   <div>
     <b-row class="w-100 mx-0 mobile-menu">
-      <b-col class="menu1 menu-icon">
-        <font-awesome-icon v-b-toggle.sidebar-menu class="ml-3 icon" icon="bars" size="lg"/>
+      <b-col v-b-toggle.sidebar-menu class="menu1 menu-icon">
+        <font-awesome-icon class="ml-3" icon="bars" size="lg"/>
       </b-col>
       <b-col class="menu2">
         <div class="logo-wrapper">
-          <b-img width="80" src="@/assets/fp-main-logo-outline.svg" class="fp-logo" @click="this.$router.push({name: 'Home'})"></b-img>
+          <b-img width="80" src="@/assets/fp-main-logo-outline.svg" class="fp-logo"
+                 @click="this.$router.push({name: 'Home'})"></b-img>
         </div>
       </b-col>
-      <b-col class="menu3 menu-icon text-right">
-        <font-awesome-icon v-b-toggle.sidebar-user icon="user-circle" class="mr-3 icon" size="lg"/>
+      <b-col v-b-toggle.sidebar-user class="menu3 menu-icon text-right">
+        <font-awesome-icon icon="user-circle" class="mr-3" size="lg"/>
       </b-col>
     </b-row>
     <b-sidebar id="sidebar-menu"
-               width="375px"
+               width="450px"
                title="Menu"
     >
-     <b-row>
-       <b-col v-for="(item, idx) in menuItems"
-              :key="item.id + idx"
-              cols="12"
-              class="text-center py-4 font-weight-bold"
-              @click="$emit('navigate',item.id)"
-              :class="item.id + '_nav'"
-       >
-         {{item.name}}
-       </b-col>
-     </b-row>
+      <b-row>
+        <b-col v-for="(item, idx) in menuItems"
+               :key="item.id + idx"
+               cols="12"
+               class="text-center py-4 font-weight-bold"
+               @click="$emit('navigate',item.id)"
+               :class="item.id + '_nav'"
+        >
+          {{ item.name }}
+        </b-col>
+      </b-row>
     </b-sidebar>
     <b-sidebar id="sidebar-user"
                title="Member Settings"
                right
-               width="375px"
-               >
+               width="450px"
+    >
       <div class="px-3 py-2">
         <div>this is the user login section</div>
       </div>
@@ -58,11 +59,10 @@ export default {
   height: 45px
 
 .menu-icon
-  padding-top: 12px
+  padding-top: 10px
 
-.icon
   &:focus
-    outline: none!important
+    outline: none !important
 
 .menu1
   background-color: #19fe77
